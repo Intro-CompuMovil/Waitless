ed707ff9c19ea6dcf5e867dec01314fb7b4f72bb
saraagutierrez <149322564+saraagutierrez@users.noreply.github.com>
2024-03-07 07:14:44 -0500

    Update TicketsActivity.kt

--- app/src/main/java/com/example/waitless_p1/TicketsActivity.kt
+++ app/src/main/java/com/example/waitless_p1/TicketsActivity.kt
@@ -26,10 +26,16 @@ class TicketsActivity : AppCompatActivity() {
             startActivity(Intent(this,HomeActivity::class.java))
         }
         findViewById<ImageButton>(R.id.iconoQR).setOnClickListener{
-            startActivity(Intent(this,ScanqrActivity::class.java))
+            startActivity(Intent(this,PermisoCamaraActivity::class.java))
         }
         findViewById<ImageButton>(R.id.iconoMapa).setOnClickListener{
             startActivity(Intent(this,MapaActivity::class.java))
+        }
+        findViewById<ImageButton>(R.id.iconoPerfil).setOnClickListener{
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+        findViewById<ImageButton>(R.id.tema).setOnClickListener{
+            Toast.makeText(this, "Sensor tema", Toast.LENGTH_LONG).show()
         }
 
         val spinner = findViewById<Spinner>(R.id.tipoEntrada)
